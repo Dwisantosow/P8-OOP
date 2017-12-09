912287707366fa128d82b981049959e7ea58051e
Dwisantosow <32090298+Dwisantosow@users.noreply.github.com>
2017-12-09 10:16:15 +0700

    Add files via upload

---
+++ Tabungan.java
@@ -0,0 +1,12 @@
+public class Tabungan {
+    int saldo;
+//method constructor
+    public Tabungan (int initsaldo){
+    saldo = initsaldo;
+    }
+//method void : tidak mengembalikan nilai
+    void ambilUang(int jumlah){
+       saldo = saldo-jumlah;
+    }
+
+}
